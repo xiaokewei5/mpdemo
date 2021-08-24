09c6d28a2acf20405b2812bf2e37437469003a82
xiaokewei5 <xiaokewei5@163.com>
2021-08-24 18:27:02 +0800

    添加转换枚举为map方法

--- src/main/java/com/example/mpdemo/common/IBaseEnum.java
+++ src/main/java/com/example/mpdemo/common/IBaseEnum.java
@@ -1,9 +1,14 @@
 package com.example.mpdemo.common;
 
 
+import cn.hutool.core.util.EnumUtil;
 import com.baomidou.mybatisplus.annotation.IEnum;
+import com.example.mpdemo.enums.DeleteEnum;
 
 import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @Author: fengyunxiaoxiao
@@ -17,5 +22,12 @@ public interface IBaseEnum<T extends Serializable> extends IEnum<T> {
     String NAME = "name";
 
     String getName();
+
+    static Map<?,String> getEnumMap(final Class clazz) {
+        LinkedHashMap<String, IBaseEnum> linkedHashMap = EnumUtil.getEnumMap(clazz);
+        Map<?, String> enumMap = linkedHashMap.entrySet().stream()
+                .collect(Collectors.toMap(a -> a.getValue().getValue(), a -> a.getValue().getName(),(a, b) -> b));
+        return enumMap;
+    }
 
 }
